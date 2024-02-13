26e357c447224170ca2864c6b0cc6f7d534ace8f
Julian Lechner <jll38@njit.edu>
2024-02-13 15:02:20 -0500

    feat(api): Create ShortURL in DB

--- src/app/api/url/create/route.ts
+++ src/app/api/url/create/route.ts
@@ -5,13 +5,42 @@ import { Shortener } from "../../../../lib/Shortener";
 
 export async function POST(req: NextRequest) {
   const { userID, originalURL, locked } = await req.json();
-  
+
   const shortURL = Shortener.shorten(originalURL);
 
+  if (await createUrlRecord(userID, originalURL, shortURL, locked))
+    return NextResponse.json(
+      { shortURL },
+      {
+        status: 200,
+      }
+    );
   return NextResponse.json(
-    { shortURL},
+    { message: "Error! Please try again later" },
     {
-      status: 200,
+      status: 400,
     }
   );
+}
+
+async function createUrlRecord(
+  userId: string,
+  originalURL: string,
+  shortURL: string,
+  locked: boolean
+): Promise<boolean> {
+  try {
+    await Prisma.getInstance().link.create({
+      data: {
+        userId,
+        originalURL,
+        shortURL: shortURL,
+        protected: locked,
+      },
+    });
+    return true;
+  } catch (err) {
+    console.error("Error: " + err);
+    return false;
+  }
 }
