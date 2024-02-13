146f74f16dfa992e81cb0ba5f5f976f9f5f98556
Julian Lechner <jll38@njit.edu>
2024-02-13 16:43:02 -0500

    Implement basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
 import Shortener from "@/components/Shortener/Shortener";
+import { Title, Text, Flex } from "@mantine/core";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Shortener />
+    <main>
+      <section style={{ height: "650px" }}>
+        <Flex
+          style={{ height: "100%", border: "1px solid"}}
+          justify="space-around"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
+          <div>
+            <Text>Welcome to</Text>
+            <Title order={1} style={{ lineHeight: "40px" }}>
+              TinyClicks
+            </Title>
+            <Text>Streamline your digital presence in one click.</Text>
+          </div>
+          <div>
+            <Shortener />
+          </div>
+        </Flex>
+      </section>
     </main>
   );
 }
