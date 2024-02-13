0c87f2e8f3dd0e3ea3d3946db3dc8e46c514ebe2
Julian Lechner <jll38@njit.edu>
2024-02-13 14:48:08 -0500

    Shortener Component with Mantine UI

--- src/components/Shortener/Shortener.tsx
+++ src/components/Shortener/Shortener.tsx
@@ -1,45 +1,57 @@
 "use client";
 import React from "react";
+import { Box, Button, TextInput, rem, Flex } from "@mantine/core";
+
 import { LinkValidator } from "@/lib/validator";
+
 export default function Shortener() {
   const userID = "657bf0e8f02c01a8e4417663"; //Temporary value
   const [link, setLink] = React.useState<string>("");
-  
+
   return (
-    <div>
-      <div>Shortener</div>
-      <input
-        type="text"
-        placeholder="https://example.com"
-        onChange={(e) => {
-          setLink(e.target.value);
-        }}
-      ></input>
-      <button
-        disabled={!LinkValidator.validate(link)}
-        onClick={() => {
-          fetch("api/url/create", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              userID,
-              originalURL: link,
-              locked: false,
-            }),
-          })
-            .then((response) => {
-              if (response.status === 200) alert("Success!");
-              else alert("Error!");
-              return response.json();
-            })
-            .then((data) => {
-              console.log(data);
-            })
-            .catch((error) => console.error("Error:", error));
-        }}
+    <Box style={{ maxWidth: 400 }}>
+      <Flex
+        mih={50}
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
       >
-        Shorten
-      </button>
-    </div>
+        <TextInput
+          leftSection={<div style={{ fontSize: 12 }}>https://</div>}
+          leftSectionWidth={"50px"}
+          type="text"
+          placeholder="example.com"
+          onChange={(e) => {
+            setLink("https://" + e.target.value);
+          }}
+        ></TextInput>
+        <Button
+          disabled={!LinkValidator.validate(link)}
+          onClick={() => {
+            fetch("api/url/create", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                userID,
+                originalURL: link,
+                locked: false,
+              }),
+            })
+              .then((response) => {
+                if (response.status === 200) alert("Success!");
+                else alert("Error!");
+                return response.json();
+              })
+              .then((data) => {
+                console.log(data);
+              })
+              .catch((error) => console.error("Error:", error));
+          }}
+        >
+          Shorten
+        </Button>
+      </Flex>
+    </Box>
   );
 }
