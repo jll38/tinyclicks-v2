1c6bd78b1960ce33e0b7a1dfea5d67c55f56ba4c
Julian Lechner <jll38@njit.edu>
2024-02-14 00:15:40 -0500

    top-performers operation route

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -2,21 +2,45 @@ import { Prisma } from "@/lib/Prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const userId = req.nextUrl.searchParams.get("usr");
-    const links = await retrieveLinks(userId)
-      return NextResponse.json(
-        { links },
-        {
-          status: 200,
-        }
-      );
-    
+  let userId = req.nextUrl.searchParams.get("usr");
+  const operation = req.nextUrl.searchParams.get("operation");
+  console.log(operation);
+  if (userId?.length === 0) userId = null;
+
+  let data;
+
+  switch (operation) {
+    case "top-performers":
+      data = await retrieveTopPerformers(userId);
+      break;
+    case "links":
+      data = await retrieveLinks(userId);
+      break;
   }
 
-async function retrieveLinks(userId : string | null){
-    return await Prisma.getInstance().link.findMany({
-        where: {
-            userId
-        }
-    });
+  return NextResponse.json(
+    { data },
+    {
+      status: 200,
+    }
+  );
+}
+
+async function retrieveTopPerformers(userId: string | null) {
+  return await Prisma.getInstance().link.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      clicks: 'desc',
+    },
+    take: 5,
+  });
+}
+async function retrieveLinks(userId: string | null) {
+  return await Prisma.getInstance().link.findMany({
+    where: {
+      userId,
+    },
+  });
 }
