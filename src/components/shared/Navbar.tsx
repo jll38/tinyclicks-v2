419d516189490e220407727ca6dc751626d6b8a8
Julian Lechner <jll38@njit.edu>
2024-02-19 00:29:53 -0500

    Improve SPA link traversing

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 import { NavLink, Text, Button, Flex, Image, Tooltip } from "@mantine/core";
 import { ActionIcon } from "@mantine/core";
+
+import { navigate } from "@/lib/navigate";
 
 export default function Navbar() {
   const navLinks = [
@@ -18,18 +21,34 @@ export default function Navbar() {
         justifyContent: "space-between",
         alignItems: "center",
         padding: "0px 20px",
-        borderBottom: "2px #64A0FF solid"
+        borderBottom: "2px #64A0FF solid",
       }}
     >
-      <Text id="nav-left" fw={600}>
-        TinyClicks
-      </Text>
+      <Button
+        variant={"transparent"}
+        c="black"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <Text id="nav-left" fw={600}>
+          TinyClicks
+        </Text>
+      </Button>
 
       <Flex id="nav-right" gap={"20"} align="center">
-        <Button component="a" variant="light" href="/login">
+        <Button
+          component="a"
+          variant="light"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
           Login
         </Button>
-        <Button component="a" variant="filled" href="/register">
+        <Button component="a" variant="filled"  onClick={() => {
+            navigate("/register");
+          }}>
           Sign Up
         </Button>
         <Tooltip label="Portfolio">
