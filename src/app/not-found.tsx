63162014c58b8e1cceb8e1b0550f457a6008d80e
Julian Lechner <jll38@njit.edu>
2024-02-19 00:35:36 -0500

    Implement 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Image, Title, Text } from "@mantine/core";
+
+export default function NotFound() {
+  return (
+    <main style={{ display: "grid", placeItems: "center", height: "85vh" }}>
+      <div>
+        <Image src={"/images/404.webp"} radius="lg" h={400} w="auto"></Image>
+        <Title order={1} ta={"center"}>Page Not Found</Title>
+      </div>
+    </main>
+  );
+}
