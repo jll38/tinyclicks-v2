7f33a7fb680efee4afe3fdfd862e25cca66fce28
Julian Lechner <jll38@njit.edu>
2024-03-17 17:34:49 -0400

    init campaign route

---
+++ src/app/dashboard/campaigns/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import UnderConstruction from '@/components/shared/placeholder/UnderConstruction/UnderConstruction'
+export default function page() {
+  return (
+    <UnderConstruction/>
+  )
+}
