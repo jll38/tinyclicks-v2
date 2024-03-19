a7a023815e8ec885a630c7cb97d72b0ed1b31884
Julian Lechner <jll38@njit.edu>
2024-03-19 19:56:10 -0400

    display top referrers in graph by color

--- src/app/dashboard/manage-links/LinkDetails.tsx
+++ src/app/dashboard/manage-links/LinkDetails.tsx
@@ -7,19 +7,21 @@ import { useSession } from "next-auth/react";
 export function LinkDetails({ selectedLink }: any) {
   const { data: session } = useSession();
   const [data, setData] = React.useState<any>();
+  const [sourceCounts, setSourceCounts] = React.useState<any>();
+
+  const colors = ["#BCE7FD", "#C492B1", "#AF3B6E", "#21FA90"]
   React.useEffect(() => {
     console.log("Fetching traffic data for " + selectedLink.name);
     //@ts-ignore
     if (session && session.user && session.user.id) {
-      
       fetch(
         //@ts-ignore
         `/api/dashboard/get-links/link?usr=${session.user.id}&link=${selectedLink.id}`
       )
         .then((res) => res.json())
         .then((data) => {
-          setData(data);
           console.log(data);
+          setSourceCounts(data.trafficSources);
         });
     }
   }, [selectedLink, session]);
@@ -36,29 +38,27 @@ export function LinkDetails({ selectedLink }: any) {
                 Edit
               </Button>
             </div>
-            <div className={styles.referrersBox}>
-              <div
-                style={{
-                  width: "30%",
-                  height: "100%",
-                  backgroundColor: "blue",
-                }}
-              ></div>
-              <div
-                style={{
-                  width: "20%",
-                  height: "100%",
-                  backgroundColor: "red",
-                }}
-              ></div>
-              <div
-                style={{
-                  width: "10%",
-                  height: "100%",
-                  backgroundColor: "pink",
-                }}
-              ></div>
-            </div>
+            {sourceCounts ? (
+              <div className={styles.referrersBox}>
+                {
+                  //@ts-ignore
+                  sourceCounts.map((source, i) => {
+                    return (
+                      <div
+                        key={"color-" + i}
+                        style={{
+                          width: `${(source._count/sourceCounts.length) * 100}%`,
+                          height: "100%",
+                          backgroundColor: colors[i],
+                        }}
+                      >{source.source || "Unknown"}</div>
+                    );
+                  })
+                }
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </>
       ) : (
