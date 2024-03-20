2ee1c0835bbab67a22f37ff32b619b20f9d400f4
Julian Lechner <jll38@njit.edu>
2024-03-19 23:45:18 -0400

    Implemented tooltip display for referrers chart

--- src/app/dashboard/manage-links/LinkDetails.tsx
+++ src/app/dashboard/manage-links/LinkDetails.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Box, Title, Text, Button } from "@mantine/core";
+import { Box, Title, Text, Button, Tooltip } from "@mantine/core";
 import styles from "./managelinks.module.css";
 import { useSession } from "next-auth/react";
 
@@ -9,7 +9,39 @@ export function LinkDetails({ selectedLink }: any) {
   const [data, setData] = React.useState<any>();
   const [sourceCounts, setSourceCounts] = React.useState<any>();
 
-  const colors = ["#BCE7FD", "#C492B1", "#AF3B6E", "#21FA90"]
+  const [tooltip, setTooltip] = React.useState({
+    visible: false,
+    content: "",
+    x: 0,
+    y: 0,
+  });
+
+  const colors = ["#BCE7FD", "#C492B1", "#AF3B6E", "#21FA90"];
+
+  const handleMouseEnter = (content: string) => (e: any) => {
+    setTooltip({
+      visible: true,
+      content,
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  const handleMouseMove = (e: any) => {
+    setTooltip((prevTooltip) => ({
+      ...prevTooltip,
+      x: e.clientX,
+      y: e.clientY,
+    }));
+  };
+
+  const handleMouseLeave = () => {
+    setTooltip((prevTooltip) => ({
+      ...prevTooltip,
+      visible: false,
+    }));
+  };
+
   React.useEffect(() => {
     console.log("Fetching traffic data for " + selectedLink.name);
     //@ts-ignore
@@ -39,22 +71,45 @@ export function LinkDetails({ selectedLink }: any) {
               </Button>
             </div>
             {sourceCounts ? (
-              <div className={styles.referrersBox}>
-                {
-                  //@ts-ignore
-                  sourceCounts.map((source, i) => {
-                    return (
-                      <div
-                        key={"color-" + i}
-                        style={{
-                          width: `${(source._count/sourceCounts.length) * 100}%`,
-                          height: "100%",
-                          backgroundColor: colors[i],
-                        }}
-                      >{source.source || "Unknown"}</div>
-                    );
-                  })
-                }
+              <div style={{margin: "20px 0"}}>
+                <Title c={"gray.7"} order={3}>Traffic Sources</Title>
+                <div className={styles.referrersBox}>
+                  {
+                    //@ts-ignore
+                  sourceCounts.map((source, i) => (
+                    <div
+                      key={"color-" + i}
+                      style={{
+                        width: `${
+                          (source._count / sourceCounts.length) * 100
+                        }%`,
+                        height: "100%",
+                        backgroundColor: colors[i],
+                      }}
+                      onMouseEnter={handleMouseEnter(
+                        `${source.source || "Unknown Origin"} - ${source._count} Clicks`
+                      )}
+                      onMouseMove={handleMouseMove}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                    </div>
+                  ))}
+                  {tooltip.visible && (
+                    <div
+                      style={{
+                        position: "fixed",
+                        left: tooltip.x + 10, // Offset from cursor
+                        top: tooltip.y + 10,
+                        backgroundColor: "white",
+                        border: "1px solid black",
+                        padding: "8px",
+                        pointerEvents: "none", // Prevents the tooltip from interfering with mouse events
+                      }}
+                    >
+                      {tooltip.content}
+                    </div>
+                  )}
+                </div>
               </div>
             ) : (
               <></>
